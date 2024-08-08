cb271918d4780df500a6f6f4a5d8f28ebc53e25b
yayaluoya <yayaluoya@sina.com>
2024-08-08 19:31:30 +0800

    -

--- init.js
+++ init.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-const reservedNames = [/^CON$/i, /^PRN$/i, /^AUX$/i, /^NUL$/i];
+/**
+ * Windows 系统中的保留设备名称，不能用作文件或文件夹的名称
+ * TODO
+ */
+const reservedNames = [/^CON$/i, /^PRN$/i, /^AUX$/i, /^NUL$/i, /^COM[1-9]$/i, /^LPT[1-9]$/i];
 
 const snippetsPath = path.join(__dirname, './snippets');
 fs.readdirSync(snippetsPath).forEach((_) => {
